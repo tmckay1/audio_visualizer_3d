ee2545bdc095198c95f5bca39ddff63d3dc98e72
Tyler McKay <tmckay1.jackson@gmail.com>
2020-12-03 01:58:57 -0500

    fix side wave

--- python/led.py
+++ python/led.py
@@ -107,7 +107,7 @@ def _update_pi():
     b = p[2][:].astype(int)
     rgb = np.bitwise_or(np.bitwise_or(r, g), b)
     # Update the pixels
-    sphere_pulse.draw_points(strip, rgb, config.N_PIXELS, p, _prev_pixels)
+    cube_side_wave.draw_points(strip, rgb, config.N_PIXELS, p, _prev_pixels)
     _prev_pixels = np.copy(p)
     strip.show()
 
