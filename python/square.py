6f2eb021a7edb2a87788b1164d23df4681f82f06
Tyler McKay <tmckay1.jackson@gmail.com>
2020-12-02 21:03:50 -0500

    refactor

--- python/square.py
+++ python/square.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def draw_points(strip, rgb, num_leds, p, prev_pixels):
-    for i in range(num_leds ** 2):
+    for i in range(num_leds):
         # Ignore pixels if they haven't changed (saves bandwidth)
         if np.array_equal(p[:, i], prev_pixels[:, i]):
             continue
