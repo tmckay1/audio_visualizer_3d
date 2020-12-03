605bdde1faa41adb2a53988e2670fc7912bc2fbd
Tyler McKay <tmckay1.jackson@gmail.com>
2020-12-02 21:25:40 -0500

    add 2d logic

--- python/cube.py
+++ python/cube.py
@@ -1,3 +1,11 @@
+import numpy as np
 
-def draw_points(strip, rgb, num_leds, prev_pixels):
-  pass
+def draw_points(strip, rgb, num_leds, p, prev_pixels):
+    for i in range(num_leds):
+        # Ignore pixels if they haven't changed (saves bandwidth)
+        if np.array_equal(p[:, i], prev_pixels[:, i]):
+            continue
+        plot_point(strip, rgb, i)
+
+def plot_point(strip, rgb, i):
+    strip._led_data[i] = int(rgb[i])
