21ca155282c052d7d8144591d6f818851f780d78
Tyler McKay <tmckay1.jackson@gmail.com>
2020-12-02 21:28:24 -0500

    bug

--- python/square.py
+++ python/square.py
@@ -2,16 +2,13 @@ import numpy as np
 
 def draw_points(strip, rgb, num_leds, p, prev_pixels):
     for i in range(num_leds ** 2):
+        odd_row = ((i // num_leds) % 2) == 1
+        if odd_row:
+            # we're snaked so the index of the rbg array is not exactly the mod of the current led position
+            index = num_leds - (i % num_leds) - 1
+        else:
+            index = i % num_leds
         # Ignore pixels if they haven't changed (saves bandwidth)
-        if np.array_equal(p[:, i], prev_pixels[:, i]):
+        if np.array_equal(p[:, index], prev_pixels[:, index]):
             continue
-        plot_point(strip, rgb, i, num_leds)
-
-def plot_point(strip, rgb, i, num_leds):
-    odd_row = ((i // num_leds) % 2) == 1
-    if odd_row:
-        # we're snaked so the index of the rbg array is not exactly the mod of the current led position
-        index = num_leds - (i % num_leds) - 1
-    else:
-        index = i % num_leds
-    strip._led_data[i] = int(rgb[index])
+        strip._led_data[i] = int(rgb[index])
