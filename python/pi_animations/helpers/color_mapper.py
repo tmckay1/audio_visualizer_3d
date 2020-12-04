e4ba7c5ca621c45f6487d44b84cd21a354987f9a
Tyler McKay <tmckay1.jackson@gmail.com>
2020-12-04 00:55:26 -0500

    fix number

--- python/pi_animations/helpers/color_mapper.py
+++ python/pi_animations/helpers/color_mapper.py
@@ -5,8 +5,8 @@ show (think green/yellow/red)
 def get_classic_color(percent_of_max):
   normalized_value = percent_of_max * 100
   if normalized_value < 50:
-    return 2 ** 6
+    return 2 ** 8
   elif normalized_value < 80:
-    return 2 ** 6
+    return 16776960
   else:
-    return 2 ** 6
+    return 2 ** 23
