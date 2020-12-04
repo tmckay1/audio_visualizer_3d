cdafdd616ea7b6e12cdeab6a5c4cff0bf0465318
Tyler McKay <tmckay1.jackson@gmail.com>
2020-12-03 22:34:27 -0500

    cleanup square

--- python/pi_animations/square.py
+++ python/pi_animations/square.py
@@ -2,14 +2,15 @@ import numpy as np
 
 def draw_points(strip, rgb, num_leds, p, prev_pixels):
     for i in range(num_leds ** 2):
-        remainder = (i % num_leds)
-        x = remainder
+        x = i % num_leds # for even y since we are in a snaking pattern
         y = i // num_leds
-        odd_y = (y % 2) == 1
-        if odd_y:
+        
+        # check if y is odd, in which case the x needs to change
+        if y % 2 == 1:
             # we're snaked so the index of the rbg array is not exactly the mod of the current led position
-            x = num_leds - remainder - 1
-            
+            # instead it's the reflection of this point across the center of the number of leds
+            x = num_leds - x - 1
+
         # Ignore pixels if they haven't changed (saves bandwidth)
         # if np.array_equal(p[:, x], prev_pixels[:, x]):
         #     continue
